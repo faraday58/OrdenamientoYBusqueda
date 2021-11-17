b3a2c64c5ca532775cc0bc3052f9ee1b006db24a
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-11-17 12:05:04 -0600

    Agregar archivos de proyecto.

---
+++ Ordenamiento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OrdenamientoYBusqueda
+{
+    class Ordenamiento
+    {
+        public static void Burbuja(int [] A )
+        {
+            int N = A.Length;  //Número de elementos del arreglo
+            
+            // A= {3,5,1,9,2,4
+            for( int j=0; j < N; j++)
+            {
+                for (int i = 0; i < N - 1; i++)
+                {
+                    //i=1                    
+                    //A[2] < A[1]
+                    // 1       5
+                    if (A[i + 1] < A[i])
+                    {
+                        //Aux = 5
+                        int Aux = A[i];
+                        //A[1] =A[2]
+                        //A[1]= 1
+                        A[i] = A[i + 1];
+                        //A[2]= 5
+                        A[i + 1] = Aux;
+
+                    }
+                }
+
+            }
+            
+
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OrdenamientoYBusqueda
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] A = { 3, 5, 1, 9, 2, 4 };
+            ImprimeMatriz(A);
+            Ordenamiento.Burbuja(A);
+            ImprimeMatriz(A);
+            Console.ReadLine();
+        }
+
+        static void ImprimeMatriz(int[] A)
+        {
+            foreach(int elemento in A)
+            {
+                Console.Write("{0}, ",elemento);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}

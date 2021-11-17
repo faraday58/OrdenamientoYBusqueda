5c91d3b3f2750ad89461a924bfe2e888276a3c01
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-11-17 12:22:58 -0600

    Busqueda

---
+++ Busqueda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenamientoYBusqueda
+{
+    class Busqueda
+    {
+        static int Directa(int[] A, int X )
+        {
+            int N = A.Length;
+            for(int i=0; i< N; i++  )
+            {
+                if(A[i] == X  )
+                {
+                    return i ;
+                }
+            }
+            return -1;
+
+        }
+
+
+
+    }
+}

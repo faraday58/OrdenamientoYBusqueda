b70e4a21a3b144b03636261e66f8589b594e58cd
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-11-17 12:46:28 -0600

    Rendimiento

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-
+using System.Diagnostics;
 namespace OrdenamientoYBusqueda
 {
     class Program
     {
         static void Main()
         {
-            int[] A = { 3, 5, 1, 9, 2, 4 };
-            ImprimeMatriz(A);
-            int indice = Busqueda.Directa(A,11);
+            //int[] A = { 3, 5, 1, 9, 2, 4 };
+            Random random = new Random();
+            int N = 5000;
+            int[] A = new int[N];
+            for(int i=0; i< N; i++)
+            {
+                A[i]= (int)(random.NextDouble() * 1000);
+            }
+             
+           // ImprimeMatriz(A);
+            /*int indice = Busqueda.Directa(A,11);
             if(indice != -1  )
             {
                 Console.WriteLine("El número buscado está en la posición: {0}", indice);
@@ -16,10 +24,17 @@ namespace OrdenamientoYBusqueda
             else
             {
                 Console.WriteLine("No se encuentra el valor en el arreglo");
-            }
+            }*/
 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            //Instrucción a medir rendimiento
             Ordenamiento.Burbuja(A);
+            sw.Stop();          
+
+
             ImprimeMatriz(A);
+            Console.WriteLine(" \n \n Tiempo de ejecución del algoritmo de burbuja es: {0} [ms]", sw.ElapsedMilliseconds);
             Console.ReadLine();
         }
 
